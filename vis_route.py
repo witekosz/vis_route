63a01e874d95d660e81b6d7232c7acf246e50bfb
witekosz <witekosz@gmail.com>
2021-02-07 23:20:17 +0100

    Add requests, remove map

--- vis_route.py
+++ vis_route.py
@@ -8,20 +8,27 @@ import subprocess
 import sys
 import urllib.request
 
-import matplotlib.pyplot as plt
-from gcmap import GCMapper
-from mpl_toolkits.basemap import Basemap
+import requests
 
-gcm = GCMapper()
+# import matplotlib.pyplot as plt
+# from gcmap import GCMapper
+# from mpl_toolkits.basemap import Basemap
 
 
-def getLoc(IP):
-    "Turn a string representing an IP address into a lat long pair"
-    # Other geolocation services are available
-    url = "https://geolocation-db.com/json/" + IP
-    response = urllib.request.urlopen(url)
-    encoding = response.info().get_content_charset("utf8")
-    data = json.loads(response.read().decode(encoding))
+# gcm = GCMapper()
+
+GEO_IP_API = "https://geolocation-db.com/json/"
+
+
+def get_loc(IP):
+    """Turn a string representing an IP address into a lat long pair"""
+
+    request = requests.get(GEO_IP_API + IP)
+    if request.status_code != 200:
+        return
+    data = request.json()
+    print(data)
+
     try:
         lat = float(data["latitude"])
         lon = float(data["longitude"])
@@ -32,7 +39,7 @@ def getLoc(IP):
         return (None, None)
 
 
-def printHelp():
+def print_help():
     print("./vis_route.py IPv4Address")
     print(" e.g. ./vis_route.py 213.138.111.222")
 
@@ -40,14 +47,14 @@ def printHelp():
 try:
     opts, args = getopt.getopt(sys.argv, "h")
 except getopt.GetoptError:
-    printHelp()
+    print_help()
     sys.exit()
 for opt, arg in opts:
     if opt == "-h":
-        printHelp()
+        print_help()
         sys.exit()
 if len(args) != 2:
-    printHelp()
+    print_help()
     sys.exit()
 IP = args[1]
 
@@ -64,9 +71,9 @@ if platform.system() == "Linux" or platform.system() == "Darwin":
         universal_newlines=True,
     )
     # plot a pretty enough map
-    fig = plt.figure(figsize=(10, 6), edgecolor="w")
-    m = Basemap(projection="mill", lon_0=0, resolution="l")
-    m.shadedrelief(scale=0.05)
+    # fig = plt.figure(figsize=(10, 6), edgecolor="w")
+    # m = Basemap(projection="mill", lon_0=0, resolution="l")
+    # m.shadedrelief(scale=0.05)
     # Where we are coming from
     lastLon = None
     lastLat = None
@@ -76,53 +83,53 @@ if platform.system() == "Linux" or platform.system() == "Darwin":
         hopIP = line.split()[1]
         if hopIP in ("*", "to"):
             continue
-        (lat, lon) = getLoc(hopIP)
+        (lat, lon) = get_loc(hopIP)
         if lat is None:
             continue
         if lastLat is not None and (lastLat - lat + lastLon - lon) != 0.0:
-            # print(lastLat,lastLon,lat,lon)
-            x, y = m(lon, lat)
-            m.scatter(x, y, 10, marker="o", color="r")
-            (line,) = m.drawgreatcircle(lastLon, lastLat, lon, lat, color="b")
+            print(lastLat,lastLon,lat,lon)
+            # x, y = m(lon, lat)
+            # m.scatter(x, y, 10, marker="o", color="r")
+            # (line,) = m.drawgreatcircle(lastLon, lastLat, lon, lat, color="b")
         lastLat = lat
         lastLon = lon
 
-    plt.tight_layout()
-    plt.show()
+    # plt.tight_layout()
+    # plt.show()
 
-elif platform.system() == "Windows":
-    proc = subprocess.Popen(
-        "C:\\Windows\\System32\\TRACERT.exe -h 25 -d -4 " + IP,
-        stdout=subprocess.PIPE,
-        shell=True,
-        universal_newlines=True,
-    )
-    fig = plt.figure(figsize=(10, 6), edgecolor="w")
-    m = Basemap(projection="mill", lon_0=0, resolution="l")
-    m.shadedrelief(scale=0.05)
-    lastLon = None
-    lastLat = None
+# elif platform.system() == "Windows":
+#     proc = subprocess.Popen(
+#         "C:\\Windows\\System32\\TRACERT.exe -h 25 -d -4 " + IP,
+#         stdout=subprocess.PIPE,
+#         shell=True,
+#         universal_newlines=True,
+#     )
+#     fig = plt.figure(figsize=(10, 6), edgecolor="w")
+#     m = Basemap(projection="mill", lon_0=0, resolution="l")
+#     m.shadedrelief(scale=0.05)
+#     lastLon = None
+#     lastLat = None
 
-    for line in proc.stdout:
-        print(line, end="")
-        if len(line.split()) != 8:
-            continue
-        else:
-            hopIP = line.split()[7]
-            if hopIP in ("*", "to"):
-                continue
-            (lat, lon) = getLoc(hopIP)
-            if lat is None:
-                continue
-            if lastLat is not None and (lastLat - lat + lastLon - lon) != 0.0:
-                x, y = m(lon, lat)
-                m.scatter(x, y, 10, marker="o", color="r")
-                (line,) = m.drawgreatcircle(lastLon, lastLat, lon, lat, color="b")
-            lastLat = lat
-            lastLon = lon
+#     for line in proc.stdout:
+#         print(line, end="")
+#         if len(line.split()) != 8:
+#             continue
+#         else:
+#             hopIP = line.split()[7]
+#             if hopIP in ("*", "to"):
+#                 continue
+#             (lat, lon) = get_loc(hopIP)
+#             if lat is None:
+#                 continue
+#             if lastLat is not None and (lastLat - lat + lastLon - lon) != 0.0:
+#                 x, y = m(lon, lat)
+#                 m.scatter(x, y, 10, marker="o", color="r")
+#                 (line,) = m.drawgreatcircle(lastLon, lastLat, lon, lat, color="b")
+#             lastLat = lat
+#             lastLon = lon
 
-    plt.tight_layout()
-    plt.show()
+#     plt.tight_layout()
+#     plt.show()
 else:
     print(
         "Sorry, this python program does not have support for your current operating system!"
